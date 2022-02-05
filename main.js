9e12f3824d86777ca6c5e410a23d6c85300924d3
Adrien Chen <cyb976933509@gmail.com>
2022-02-05 16:22:44 -0500

    implemented timer

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
+const time_el = document.querySelector('.watch .time');
+const start_btn = document.getElementById('start');
+const stop_btn = document.getElementById('stop');
+const reset_btn = document.getElementById('reset');
+
+let seconds = 0;
+let interval = null;
+
+
+
+
 window.addEventListener('load', ()=>{
     const form = document.querySelector('#new-task-form');
     const input = document.querySelector('#new-task-input');
     const list_el = document.querySelector('#tasks');
 
+      start_btn.addEventListener('click', start);
+      stop_btn.addEventListener('click', _stop);
+      reset_btn.addEventListener('click', reset);
+
+
     form.addEventListener('submit', (e)=>{
-        e.preventDefault(); 
+        e.preventDefault();
 
         const task = input.value;
 
         if (!task){
             alert("please fill out the task");
-            return; 
+            return;
         }
         const task_el = document.createElement("div");
         task_el.classList.add("task");
@@ -21,9 +37,9 @@ window.addEventListener('load', ()=>{
         task_el.appendChild(task_content_el);
 
         const task_input_el = document.createElement("input");
-        task_input_el.classList.add("text"); 
+        task_input_el.classList.add("text");
         task_input_el.type = "text";
-        task_input_el.value= task; 
+        task_input_el.value= task;
         task_input_el.setAttribute("readonly", "readonly");
 
         task_content_el.appendChild(task_input_el);
@@ -65,3 +81,44 @@ window.addEventListener('load', ()=>{
         });
     })
 })
+
+
+function timer(){
+    seconds++;
+
+    let hrs = Math.floor(seconds /3600);
+    let mins = Math.floor((seconds - (hrs*3600))/ 60);
+    let secs = seconds % 60;
+
+    if (secs < 10){
+        secs = '0' + secs;
+    }
+
+    if (mins < 10){
+        mins = '0' + mins;
+    }
+    if (hrs < 10){
+        hrs = '0' + hrs;
+    }
+
+    time_el.innerText = `${hrs}:${mins}:${secs}`;
+}
+
+function start() {
+    if (interval){
+        return
+    }
+    interval = setInterval(timer, 1000);
+
+}
+
+function reset(){
+    _stop();
+    seconds = 0;
+    time_el.innerText = '00:00:00';
+}
+
+function _stop(){
+    clearInterval(interval);
+    interval= null;
+}
